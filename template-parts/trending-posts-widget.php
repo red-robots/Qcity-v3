aebfe948bf38a91519f1a274d6e867393415cd30
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-02-22 14:33:14 -0500

    changed time range for popular posts on trending widget

--- template-parts/trending-posts-widget.php
+++ template-parts/trending-posts-widget.php
@@ -10,7 +10,7 @@ if( do_shortcode('[wpp range="last5days" thumbnail_width=300 thumbnail_height=20
 				<div id="sbContent">
 					<div class="sbWrap">
 						<h3 class="sbTitle">Trending</h3>
-						<?php echo do_shortcode('[wpp range="last5days" thumbnail_width=300 thumbnail_height=200 limit=5 stats_views=0 order_by="views"]'); ?>
+						<?php echo do_shortcode('[wpp range="last24hours" thumbnail_width=300 thumbnail_height=200 limit=5 stats_views=0 order_by="views"]'); ?>
 					</div>
 				</div>
 			</div>
