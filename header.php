f109e306087ae6e8a5b789d2479a306ff85837b0
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-05-15 01:44:31 -0400

    Fixed header script from ACF not showing up

--- header.php
+++ header.php
@@ -19,42 +19,34 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 <script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
-<?php 
-if( $ads_scripts = getHeaderScripts() ) {
-  foreach($ads_scripts as $js) {
-    echo $js;
-  }
-}
-?>
-
-<!-- <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
 <script>
   var googletag = googletag || {};
   googletag.cmd = googletag.cmd || [];
 </script>
 <script>
-window.googletag = window.googletag || {cmd: []};
-  googletag.cmd.push(function() {
+  window.googletag = window.googletag || {cmd: []};
+    googletag.cmd.push(function() {
     googletag.defineSlot('/1009068/In-Story_Gutenbers', [300, 250], 'div-gpt-ad-1563273598512-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
   });
   
  window.googletag = window.googletag || {cmd: []};
-  googletag.cmd.push(function() {
+    googletag.cmd.push(function() {
     googletag.defineSlot('/1009068/Gutenburg_720x90', [728, 90], 'div-gpt-ad-1563293411913-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
   });
 
-    window.googletag = window.googletag || {cmd: []};
-  googletag.cmd.push(function() {
+  window.googletag = window.googletag || {cmd: []};
+    googletag.cmd.push(function() {
     googletag.defineSlot('/1009068/600x200', [600, 200], 'div-gpt-ad-1563224728772-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
   });
-</script> -->
+</script>
 
 <!-- <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
 <script>
@@ -65,6 +57,13 @@ window.googletag = window.googletag || {cmd: []};
     googletag.enableServices();
   });
 </script> -->
+<?php 
+if( $ads_scripts = getHeaderScripts() ) {
+  foreach($ads_scripts as $js) {
+    echo $js;
+  }
+}
+?>
 <script>
 var ajaxURL = "<?php echo admin_url('admin-ajax.php'); ?>";
 var assetsDIR = "<?php echo get_bloginfo("template_url") ?>/images/";
