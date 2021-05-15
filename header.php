705d80e1582cebce4df6fb5894d3fb9fba42d9e5
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-05-15 01:39:33 -0400

    Fixed header script from ACF not showing up

--- header.php
+++ header.php
@@ -19,8 +19,15 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 <script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
+<?php 
+if( $ads_scripts = getHeaderScripts() ) {
+  foreach($ads_scripts as $js) {
+    echo $js;
+  }
+}
+?>
 
-<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+<!-- <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
 <script>
   var googletag = googletag || {};
   googletag.cmd = googletag.cmd || [];
@@ -47,7 +54,7 @@ window.googletag = window.googletag || {cmd: []};
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
   });
-</script>
+</script> -->
 
 <!-- <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
 <script>
@@ -103,14 +110,6 @@ html body.single-post .oakland-background.oakland-optin-visible.oakland-lightbox
 body.single-post .oakland-background.oakland-optin-visible.oakland-lightbox{display:none!important;}
 .gform_wrapper ul li.gfield{clear: none !important;}
 </style>
-
-<?php 
-if( $ads_scripts = getHeaderScripts() ) {
-  foreach($ads_scripts as $js) {
-    echo $js;
-  }
-}
-?>
 </head>
 <?php
 $dd = date('d') - 1;
