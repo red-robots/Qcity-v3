1c48fbba6c1d5181caa9ceffa7dcafaf5bc6fd89
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-05-18 03:20:47 -0400

    Modified comment section on single post

--- template-parts/content.php
+++ template-parts/content.php
@@ -174,8 +174,47 @@ if( !defined('HIDE_ADS') ){
 
 			<?php } ?>
 
+			<?php if ( comments_open() || get_comments_number() ) { ?>
+				<?php 
+					$commentCount = get_comments_number();
+					$commentText = ($commentCount>1) ? ' Comments':' Comment'; 
+					$isCommentZero = ($commentCount==0) ? 'nocomments':'hascomments';
+				?>
+				<div class="comments-section">
+					<div class="comments-trigger">
+						<div class="logo-holder" style="display:none;">
+							<img src="<?php bloginfo('template_url'); ?>/images/qc-logo.png" alt="">
+						</div>
+						<div class="text-holder">
+							<div style="display:none;">
+								<p><?php echo $single_post_comment_text; ?>  <a id="commentBtn" class="click_class" >Click here</a></p>
+							</div>
+							<a href="#" id="commentInfoBtn"><span class="commentTxt"><span class="icon"><i class="far fa-comment"></i></span> <?php echo $commentCount.$commentText ?></span></a>
+							
+						</div>
+					</div>
 
-			<?php /* SUBSCRIPTION FORM */ 
+					<div class="comments-block <?php echo $isCommentZero ?>" style="display:block;">
+						<?php 
+							// If comments are open or we have at least one comment, load up the comment template.
+							comments_template();			
+						?>
+					</div>			
+				</div>
+			<?php }  ?>
+
+			<?php if( has_tag() ) { ?>
+				<div class="tags">	
+					 <?php echo get_the_tag_list(
+					 	'<span class="title">This Story is Tagged: </span> ',
+					 	', '
+					 ); ?>
+				</div>
+			<?php } ?>
+
+
+
+			<?php /*===== SUBSCRIPTION FORM =====*/ 
 			$formPostId = get_field("embedForms");
 			$formVisibility = get_field("show_hide_form");
 			$showForm = ($formVisibility=='hide') ? false : true;
@@ -214,44 +253,9 @@ if( !defined('HIDE_ADS') ){
 				<div class="formDiv default"><?php get_template_part( 'home-parts/subscribe-form'); ?></div>
 			</div>
 			<?php } ?>
+			
+			<?php /*===== end of SUBSCRIPTION FORM =====*/ ?>
 
-			<?php if ( comments_open() || get_comments_number() ) { ?>
-				<?php 
-					$commentCount = get_comments_number();
-					$commentText = ($commentCount>1) ? ' Comments':' Comment'; 
-					$isCommentZero = ($commentCount==0) ? 'nocomments':'hascomments';
-				?>
-				<div class="comments-section">
-					<div class="comments-trigger">
-						<div class="logo-holder" style="display:none;">
-							<img src="<?php bloginfo('template_url'); ?>/images/qc-logo.png" alt="">
-						</div>
-						<div class="text-holder">
-							<div style="display:none;">
-								<p><?php echo $single_post_comment_text; ?>  <a id="commentBtn" class="click_class" >Click here</a></p>
-							</div>
-							<a href="#" id="commentInfoBtn"><span class="commentTxt"><span class="icon"><i class="far fa-comment"></i></span> <?php echo $commentCount.$commentText ?></span></a>
-							
-						</div>
-					</div>
-
-					<div class="comments-block <?php echo $isCommentZero ?>" style="display:block;">
-						<?php 
-							// If comments are open or we have at least one comment, load up the comment template.
-							comments_template();			
-						?>
-					</div>			
-				</div>
-			<?php }  ?>
-
-			<?php if( has_tag() ) { ?>
-				<div class="tags">	
-					 <?php echo get_the_tag_list(
-					 	'<span class="title">This Story is Tagged: </span> ',
-					 	', '
-					 ); ?>
-				</div>
-			<?php } ?>
 
 			<?php 
 			$postid = get_the_ID();
