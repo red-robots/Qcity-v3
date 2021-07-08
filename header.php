011243506934cf68cc1a4649a1112a76ddc6f106
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-07-08 00:26:35 -0400

    Fixed google ad not showing up on sticky ad area on footer

--- header.php
+++ header.php
@@ -19,11 +19,18 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 <script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
+<?php 
+$stickyHeaderCode = get_field("stickyHeaderCode","option"); 
+$stickyAdCode = get_field("stickyAdCode","option");
+$stickyAdEnable = get_field("stickyAdEnable","option");
+$is_sticky_on = ( isset($stickyAdEnable) && $stickyAdEnable=='on' ) ? true : false;
+?>
 <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
 <script>
   var googletag = googletag || {};
   googletag.cmd = googletag.cmd || [];
 </script>
+<?php if ($stickyHeaderCode) { echo $stickyHeaderCode; } ?>
 <script>
   window.googletag = window.googletag || {cmd: []};
     googletag.cmd.push(function() {
@@ -138,14 +145,6 @@ body.single-post .oakland-background.oakland-optin-visible.oakland-lightbox{disp
 if( $customHeadScripts = get_field("custom_scripts_inside_head","option") ) { 
   echo $customHeadScripts;
 } ?>
-
-<?php 
-$stickyHeaderCode = get_field("stickyHeaderCode","option"); 
-$stickyAdCode = get_field("stickyAdCode","option");
-$stickyAdEnable = get_field("stickyAdEnable","option");
-$is_sticky_on = ( isset($stickyAdEnable) && $stickyAdEnable=='on' ) ? true : false;
-if ($stickyHeaderCode) { echo $stickyHeaderCode; }
-?>
 </head>
 <?php
 $dd = date('d') - 1;
